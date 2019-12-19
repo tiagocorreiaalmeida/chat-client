a107075cad1f00c0f54ead1c90ab9ab1410370bc
tiagoalmeida.correia <tiagoalmeida.correia@gmail.com>
2019-12-19 22:27:53 +0000

    Configuration of typescript, babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+const appPath = path.resolve(__dirname, "src");
+
+module.exports = env => {
+  const { prod = false } = env;
+
+  return {
+    mode: prod ? "production" : "development",
+    entry: appPath,
+
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist")
+    },
+
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"]
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/,
+          loader: "babel-loader",
+          exclude: /node_modules/
+        }
+      ]
+    },
+    devServer: {
+      port: 9000,
+      open: true
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: true,
+        template: path.join(appPath, "index.html")
+      }),
+      new ForkTsCheckerWebpackPlugin()
+    ]
+  };
+};
